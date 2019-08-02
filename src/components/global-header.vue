4e922df792ee847c102df2baa565d24b96e26bca
MagixSui <suixin@jcinfo.online>
2019-08-02 14:58:29 +0800

    first commit

---
+++ src/components/global-header.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-layout-header class="header">
+    <button @click="toggle">toggle</button>
+  </a-layout-header>
+</template>
+
+<script>
+
+export default {
+  name: 'global-header',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+      toggle() {
+          this.$emit('toggle')
+      }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/components/menu/side-menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-layout-sider
+    v-model="collapsed"
+  >
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+    <div>1</div>
+  </a-layout-sider>
+</template>
+
+<script>
+
+export default {
+  name: 'side-menu',
+  props: {
+    collapsed: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
