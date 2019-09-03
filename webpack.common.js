3482f88d4b126f9d1af56fb9ca1c96dbb2d7bd5d
magixsui <sx32612254@gmail.com>
2019-09-03 09:17:54 +0800

    multiple mode

--- webpack.common.js
+++ webpack.common.js
@@ -7,11 +7,37 @@ module.exports = {
         app: './src/index.js'
     },
     output: {
-        filename: '[name].bundle.js', 
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader', options: { injectType: 'linkTag' } },
+                    { loader: 'file-loader' }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/[name].[ext]?[hash]'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            }
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+          })
     ]
 };
